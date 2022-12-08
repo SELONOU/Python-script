298f92872caa9ab0a82232976cef92046f0d7fa8
KANKİNOU SELONOU GAUTİER <kankinounoatedegautier@yahoo.fr>
2022-12-08 17:25:00 +0300

    Update split_column_to_two_col.py

--- split_column_to_two_col.py
+++ split_column_to_two_col.py
@@ -6,3 +6,12 @@ df = pd.read_csv("all_results_smina_pubchem_last.csv")
 df[['BindingDB','Pubchem_CID']] = df["BindingDB_Pubchem"].str.split("_",expand=True)
 
 df.to_csv("perş.csv")
+
+
+
+## groupby 
+
+import pandas as pd
+df =pd.read_csv("all_tanimoto_results_more_7.csv")
+df2 = df.groupby("CID_PubChem").agg(list).to_csv("output_per.csv", sep =",")
+#df2 = df.groupby("CID_PubChem").agg(list)
